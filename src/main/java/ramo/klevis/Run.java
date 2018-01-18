fcd6a3461c228e0f9c43700a24b79ad5d867606d
klevis.ramo <klevisgramo@gmail.com>
2018-01-18 22:47:51 +0100

    stop the video

--- src/main/java/ramo/klevis/Run.java
+++ src/main/java/ramo/klevis/Run.java
@@ -1,11 +1,28 @@
 package ramo.klevis;
 
+import javax.swing.*;
+import java.util.concurrent.Executors;
+
 /**
  * Created by klevis.ramo on 1/18/2018.
  */
 public class Run {
-
+    private static JFrame mainFrame = new JFrame();
     public static void main(String[] args) throws Exception {
-        new UI().initUI();
+
+        ProgressBar progressBar = new ProgressBar(mainFrame, true);
+        progressBar.showProgressBar("Loading model this make take several seconds!");
+        UI ui = new UI();
+        Executors.newCachedThreadPool().submit(()->{
+            try {
+                TinyYoloPrediction.getINSTANCE();
+                ui.initUI();
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            } finally {
+                progressBar.setVisible(false);
+                mainFrame.dispose();
+            }
+        });
     }
 }
